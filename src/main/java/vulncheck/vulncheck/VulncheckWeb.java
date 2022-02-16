69d21080a22eefacde340292ee00aa999959f3f3
Sam Bultitude <Sam.Bultitude@gamma.co.uk>
2022-02-16 12:06:27 +0000

    Add

---
+++ src/main/java/vulncheck/vulncheck/VulncheckWeb.java
@@ -0,0 +1,32 @@
+package vulncheck.vulncheck;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class VulncheckWeb {
+	private VulncheckDB db;
+	private static Logger LOG = LoggerFactory.getLogger(VulncheckWeb.class);
+
+    @Autowired
+	public VulncheckWeb(VulncheckDB db){
+		this.db = db;
+	}
+
+	@GetMapping("/{in}")
+	public String get(@PathVariable String in){
+		LOG.info(in);
+		return db.get(in);
+	}
+
+	@GetMapping("/a/{in}")
+	public String get(@PathVariable String in){
+		LOG.info(in);
+		return db.get2(in);
+	}
+
+}
